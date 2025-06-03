4f7161fdbe75219cd4ff501145e66de1de6f6826
AbinayaJ <abinaya@thanjaicloudtech.com>
2025-06-03 15:59:12 +0530

    Update

--- src/app/login-page/login-page.ts
+++ src/app/login-page/login-page.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login-page.html',
-  styleUrl: './login-page.scss'
+  styleUrls: ['./login-page.scss']
 })
 export class LoginPage {
+
+  constructor(private router: Router) {}
+
+  submit(){
+    localStorage.setItem('isLoggedIn', 'true');
+  }
 
 }
