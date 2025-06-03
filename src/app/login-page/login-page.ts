674209a0d1280176323aa42b2a008c767924ab6c
AbinayaJ <abinaya@thanjaicloudtech.com>
2025-06-03 16:33:19 +0530

    Commit

--- src/app/login-page/login-page.ts
+++ src/app/login-page/login-page.ts
@@ -7,31 +7,31 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-login-page',
   imports: [CommonModule, FormsModule, HttpClientModule],
-  standalone: true,
   templateUrl: './login-page.html',
   styleUrls: ['./login-page.scss']
 })
 export class LoginPage {
 
+  message: string = '';
   Api_URL = 'http://localhost:3000/api/v1/user/login';
   user = {
-    email: 'kjajaykumar8307@gmail.com',
-    password: 'Ajay'
+    email: '',
+    password: ''
   };
-  message: string = '';
 
   constructor(private router: Router, private http: HttpClient) {}
 
   submit(){
-    this.http.post<any>(this.Api_URL, this.user).subscribe(res => {
-      this.message = res.message;
-      console.log('Login successful', res);
-      setTimeout(() => {
-        this.router.navigate(['/']);
-      }, 2000);
-    }, error => {
-      this.message = error.error.message;
-      console.error('Login failed', error);
-    });
+    this.http.post<any>(this.Api_URL, this.user).subscribe(
+      {
+        next: (res: any) => {
+          this.message = res.message;
+        },
+        error: (error) => {
+          this.message = error.error.message;
+          console.error('Login failed', error);
+        }
+      }
+    );
   }
 }
