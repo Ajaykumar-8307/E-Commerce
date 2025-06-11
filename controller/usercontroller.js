b38fbcc969482e5ffe677de810aec51170f1afca
Ajaykumar-8307 <153371277+Ajaykumar-8307@users.noreply.github.com>
2025-06-11 12:10:56 +0000

    delete user

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -96,6 +96,20 @@ exports.LoginUser = async (req,res) => {
     }
 }
 
+exports.delUser = async (req,res) => {
+    const { email } = req.body;
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(500).json({message: "Given User Not Exist"});
+        }
+        await User.deleteMany({email});
+        return res.status(200).json({message: "Account Deleted Successfully"});
+    } catch (error){
+        return res.status(400).json({message: "Error to deleting User"});
+    }
+}
+
 exports.getUser = (req, res) => {
     res.status(200).json({ message: 'Welcome to your profile', user: req.user });
 }
--- router/userrouter.js
+++ router/userrouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, LoginUser, getUser, verifyOtp, resendOtp} = require('../controller/usercontroller');
+const {registerUser, LoginUser, delUser,getUser, verifyOtp, resendOtp} = require('../controller/usercontroller');
 const authenticate = require('../middleware/authentication');
 
 router.post('/register', registerUser);
 router.post('/login', LoginUser);
 router.post('/verify', verifyOtp);
 router.post('/resendotp', resendOtp);
+router.post('/deluser', delUser);
 
 router.get('/getuser', authenticate, getUser);
 
