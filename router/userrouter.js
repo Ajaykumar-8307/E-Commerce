1edbdfb9917fc34d88d0b8cd3de2a45fb082e3e2
AbinayaJ <abinaya@thanjaicloudtech.com>
2025-06-14 15:52:01 +0530

    update

--- router/userrouter.js
+++ router/userrouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {registerUser, LoginUser, changePass, delUser,getUser, getUserProfile, verifyOtp, resendOtp, changeDetails} = require('../controller/usercontroller');
 const authenticate = require('../middleware/authentication');
 
+//User Controllers Post Methods
 router.post('/register', registerUser);
 router.post('/login', LoginUser);
 router.post('/verify', verifyOtp);
@@ -10,8 +11,10 @@ router.post('/resendotp', resendOtp);
 router.post('/deluser', delUser);
 router.post('/changepass', changePass);
 
+//Put Methods
 router.put('/update-profile', changeDetails);
 
+//Get Methods
 router.get('/getuser', getUser);
 router.get('/get-user-details', getUserProfile);
 
