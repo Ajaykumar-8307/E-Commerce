fbc10e3df57346346c862aad43d2c7f2ce42a8b4
AbinayaJ <abinaya@thanjaicloudtech.com>
2025-06-18 17:57:24 +0530

    edit products

--- controller/productcontroller.js
+++ controller/productcontroller.js
@@ -64,7 +64,7 @@ exports.editProducts = async (req, res) => {
   const { id } = req.body;
   try{
     const product = await Product.findById(id);
-    return res.status(200).json({message:"Product Find", product});
+    return res.status(200).json(product);
   } catch (error) {
     return res.status(400).json({message: "Error to fetch Data"});
   }
