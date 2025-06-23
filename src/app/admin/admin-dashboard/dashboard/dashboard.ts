84b0dce04bb3a214c3b89055f3ad1946497c77f8
AbinayaJ <abinaya@thanjaicloudtech.com>
2025-06-23 13:12:43 +0530

    get product length

--- src/app/admin/admin-dashboard/dashboard/dashboard.ts
+++ src/app/admin/admin-dashboard/dashboard/dashboard.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from '../../../../environments/environments.prod';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
@@ -12,15 +14,23 @@ export class Dashboard implements OnInit {
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private cd: ChangeDetectorRef
   ) { }
 
   token: any = '';
+  API_Link = environment.apiUrl;
+
+  products: any[] = [];
 
   admin = {
-    name: String,
-    isAdmin: false
+    name: '',
+    isAdmin: false,
+    id: ''
   }
+
+  lenght: Number = 0;
 
   ngOnInit(): void {
     if (typeof window !== 'undefined') {
@@ -30,6 +40,7 @@ export class Dashboard implements OnInit {
         if (deCodedToken) {
           this.admin.isAdmin = deCodedToken.isAdmin;
           this.admin.name = deCodedToken.userName;
+          this.admin.id = deCodedToken.id;
           console.log(this.admin.isAdmin);
         }
         if (!this.admin.isAdmin) {
@@ -40,7 +51,22 @@ export class Dashboard implements OnInit {
         alert('Login First');
         this.router.navigate(['/login']);
       }
+
+      this.http.get<any[]>(`${this.API_Link}/product/adminproducts`, { params: { adminId: this.admin.id } }).subscribe({
+        next: (res: any) => {
+          this.products = res;
+          console.log(res);
+          this.cd.detectChanges();
+        },
+        error: (error: any) => {
+          alert(`${error.error.message}, Try Again Later`);
+        }
+      });
     }
   }
 
+  getProductCount(): Number {
+    this.lenght = this.products.length;
+    return this.lenght;
+  }
 }
