e643a48fe315dc06d21741afe9afde19300fdffa
AbinayaJ <abinaya@thanjaicloudtech.com>
2025-06-26 15:11:11 +0530

    Buy Now

--- router/paymentrouter.js
+++ router/paymentrouter.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { Pay } = require('../controller/paymentcontroller');
 
-router.post('/create-checkout-session', Pay);
+router.post('/buynow', Pay);
 
 module.exports = router;
