9046072dece9384a99ccc0e5160fc1a53fc54026
Ajaykumar-8307 <153371277+Ajaykumar-8307@users.noreply.github.com>
2025-06-26 14:36:48 +0530

    Update paymentcontroller.js

--- controller/paymentcontroller.js
+++ controller/paymentcontroller.js
@@ -1,26 +1,30 @@
+const Stripe = require("stripe");
+const stripe = Stripe("sk_test_51Re964QDrlAoNSk6ADlYvhC09OxB1fnDK6HtFM6NYwPyfZf00ehqRNKwdeTTVkOT4Zol5uWZnPCSxVBGsX8rHeJu00Mx7tUFoB");
+
 exports.Pay = async (req, res) => {
+    const { product } = req.body;
+
     try {
         const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            mode: "payment",
-            line_items: [
-                {
-                    price_data: {
-                        currency: "inr",
-                        product_data: {
-                            name: "Test Product",
-                        },
-                        unit_amount: 10000, // ₹100.00 in paise
-                    },
-                    quantity: 1,
+            payment_method_types: ['card'],
+            mode: 'payment',
+            line_items: [{
+            price_data: {
+                currency: 'inr',
+                product_data: {
+                name: product.name,
                 },
-            ],
-            success_url: "http://localhost:5000/success.html",
-            cancel_url: "http://localhost:5000/cancel.html",
+                unit_amount: product.price * 1000,
+            },
+            quantity: 1,
+            }],
+            success_url: 'http://localhost:4200/success',
+            cancel_url: 'http://localhost:4200/cancel',
         });
 
-        res.json({ url: session.url });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+        return res.status(200).json({message: "Order Placed Successfully", id: session.id });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Stripe error');
     }
 }
